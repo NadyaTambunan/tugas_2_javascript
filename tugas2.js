99326c412d8f2d222f77b5d72c66f28e733dc412
NadyaTambunan <nadiaputri99tambunan@gmail.com>
2022-12-01 14:15:16 +0700

    Tugas 2 Js
    Tugas Niomic

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var tipe1 = 2;
+var tipe2 = "Nama";
+var tipe3 = 3.4;
+var tipe4 = null;
+var tipe5 = [1,2,3,4,5]
+
+console.log(tipe1);
+console.log(tipe2);
+console.log(tipe3);
+console.log(tipe4);
+console.log(tipe5);
